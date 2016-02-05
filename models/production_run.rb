8e344df0f59f9f82b7ef7dc29370cdf6b7df2377
cmthakur <cmthakur.r2s@gmail.com>
2016-02-05 17:44:32 +0545

    add create method for production run

--- models/production_run.rb
+++ models/production_run.rb
@@ -1,7 +1,26 @@
 class ProductionRun
-  def initialize(args)
-
+  def initialize(args={})
+    @params = args
+    @line_id = args[:line_id] || args[:template_id]
   end
 
+  def save
+    created_at = Time.now.utc
+    access_hash = "#{@line_id}-#{created_at.to_i}"
+    @params = @params.merge(created_at: created_at)
+    redis.lpush @line_id, @params.to_json
+    ::Relay::Producer.publish('client.create_run', @params)
 
+    {
+      id: access_hash,
+      line_id: @line_id,
+      status: "Processing",
+      created_at: created_at
+    }
+  end
+
+  def self.create(params)
+    run = ProductionRun.new(params)
+    run.save
+  end
 end
