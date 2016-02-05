dce1f1d325f49a03b5666f1dd3d1cf4648c56d74
cmthakur <cmthakur.r2s@gmail.com>
2016-02-05 14:36:50 +0545

    rename app

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
 I18n.enforce_available_locales = false
 
-module CfReceiver
+module RelayApp
   class App < Grape::API
     mount API::V1::Base
   end
--- config.ru
+++ config.ru
@@ -2,4 +2,4 @@ Bundler.require
 require 'rack/contrib/try_static'
 require File.join(File.dirname(__FILE__), 'app.rb')
 
-run CfReceiver::App
+run RelayApp::App
