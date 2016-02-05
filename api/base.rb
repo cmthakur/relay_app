522ced23e82e53c1454c293318b567c43f95a0e4
cmthakur <cmthakur.r2s@gmail.com>
2016-02-05 17:42:25 +0545

    add production runs controller

--- api/base.rb
+++ api/base.rb
@@ -1,4 +1,7 @@
 Dir["#{File.dirname(__FILE__)}/*.rb"].each {|file| require(file)}
+require_relative "v1/production_runs_controller"
+
+require 'grape-swagger'
 require 'grape-swagger'
 require 'digest/sha1'
 
@@ -9,11 +12,7 @@ module API
       format :json
       version 'v1'
 
-
-      helpers do
-      end
-
-      # mount API::V1::RunsController
+      mount API::V1::ProductionRunsController
 
       add_swagger_documentation api_version: 'v1',
         mount_path: 'docs',
