f98b674fe7756431bf7d9095fb9159f6014c3b52
cmthakur <cmthakur.r2s@gmail.com>
2016-02-08 11:58:50 +0545

    add api for v4 run

---
+++ api/v4/production_runs_controller.rb
@@ -0,0 +1,40 @@
+require_relative '../../lib/run_utility'
+Dir["#{File.dirname(__FILE__)}/*.rb"].each {|file| require(file)}
+require 'grape-swagger'
+require 'grape-swagger'
+require 'digest/sha1'
+
+module API
+  module V4
+    class ProductionRunsController < Grape::API
+      include RunUtility
+
+      resource :runs do
+        desc 'this route description'
+        get '/' do
+        end
+
+        desc 'this route description'
+        post '/' do
+          parameter = params.merge({version: 'v4'})
+          response = RunUtility.verify_request(parameter)
+          # if response.blank?
+          response = ProductionRun.new(parameter).save
+          # end
+
+          response
+        end
+      end
+
+
+      prefix 'api'
+      format :json
+      version 'v4'
+      add_swagger_documentation api_version: 'v4',
+        mount_path: 'docs',
+        hide_format: true,
+        hide_documentation_path: true
+
+    end
+  end
+end
