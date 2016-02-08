d2650f0849b464bd100ffc6907feef18123563b0
cmthakur <cmthakur.r2s@gmail.com>
2016-02-08 11:59:07 +0545

    v3 and v4 responses

--- models/production_run.rb
+++ models/production_run.rb
@@ -1,22 +1,32 @@
 class ProductionRun
   def initialize(args={})
     @params = args
+    @version = args[:version] || 'v4'
     @line_id = args[:line_id] || args[:template_id]
   end
 
   def save
     created_at = Time.now.utc
     access_hash = "#{@line_id}-#{created_at.to_i}"
-    @params = @params.merge(created_at: created_at)
+    @params = @params.merge(created_at: created_at, access_hash: access_hash, version: @version)
     redis.lpush @line_id, @params.to_json
     ::Relay::Producer.publish('client.create_run', @params)
 
-    {
-      id: access_hash,
-      line_id: @line_id,
-      status: "Processing",
-      created_at: created_at
-    }
+    if @version == 'v4'
+      {
+        id: access_hash,
+        line_id: @line_id,
+        status: "Processing",
+        created_at: created_at
+      }
+    else
+      {
+        task_id: access_hash,
+        template_id: @line_id,
+        status: "Processing",
+        started_at: created_at
+      }
+    end
   end
 
   def self.create(params)
